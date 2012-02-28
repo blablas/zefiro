a6d2fbd9ee3f25c82345f099fc6286ca364ff99c
giovannetti.emiliano@gmail.com <giovannetti.emiliano@gmail.com@8e901620-6d84-a92a-eb7a-39a5d1e1b6b3>
2012-02-28 09:44:00 +0000

    Add daemon skeleton, plc API and basic test

---
+++ trunk/include/readPLC.h
@@ -0,0 +1,31 @@
+/*
+** Copyright (C) 2012 Emiliano Giovannetti <emiliano.giovannetti@tdt.it>
+**  
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software 
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+#ifndef READPLC_H
+#define READPLC_H
+#include <nodave.h>
+#include <openSocket.h>
+
+extern int plcConnect (const int i, daveConnection **dc, daveInterface **di);
+extern int plcDisconnect (daveConnection **dc, daveInterface **di);
+extern int plcRead (daveConnection *dc, 
+		    int area, 
+		    int db, 
+		    int start, 
+		    int len, 
+		    void *buffer);
+#endif
